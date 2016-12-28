09e0ed184c751a4decc0eadd6f8417f8370a6ce8
Els <vanpnaarp@gmail.com>
2016-12-28 14:34:42 +0100

    my assignment, please check

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,32 @@
 
 ## Write a short comment describing this function
 
+
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv_els=NULL
+        set <- function(y){
+                x <<-y
+                inv_els <<- NULL
+        }
+        get <- function() x
+        setInverse<- function(Inverse) inv_els <<-Inverse
+        getInverse <- function() inv_els
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
-
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv_els <- x$getInverse()
+        if(!is.null(inv_els)) {
+                message("Get cache data for Elsa")
+                return(inv_els)
+        }
+        else{
+                data <- x$get()
+                inv_els <- solve(data)
+                x$setInverse(inv_els)
+                inv_els
+        }
 }
+
