559d1682e56e6ab7152e5dac79da55b2c9aa98ae
Els <vanpnaarp@gmail.com>
2016-12-29 16:33:10 +0100

    this is my updated submission after the comments
    after the comments of my peers (my own comments were missing in the original R code that I submitted)

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming assignment 2
 
-## Write a short comment describing this function
-
+##this function is at first creating the matrix and cache its inverse
+##
 
 makeCacheMatrix <- function(x = matrix()) {
-        inv_els=NULL
+        inv_els <- NULL
         set <- function(y){
                 x <<-y
                 inv_els <<- NULL
@@ -16,19 +15,26 @@ makeCacheMatrix <- function(x = matrix()) {
         list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-## Write a short comment describing this function
+
+## This function computes the inverse of the matrix returned by makeCacheMatrix (see above).
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cacheSolve should retrieve the inverse from the cache. If so, the text
+## "Get cache data for Elsa" will appear
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inv_els <- x$getInverse()
         if(!is.null(inv_els)) {
-                message("Get cache data for Elsa")
+                message("Get cache data for Els")
                 return(inv_els)
-        }
+        } 
+        ## this checks if the inverse of matrix 'x' is already available in cache
         else{
                 data <- x$get()
                 inv_els <- solve(data)
                 x$setInverse(inv_els)
                 inv_els
         }
+        ## and this will compute the inverse if it doesn't excist in cache already.
 }
 
